8d020afc5ed3fab3fb467fa96c2b431cb61b19de
ThanhToan <codehub.personal@gmail.com>
2024-02-22 14:52:13 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const cors = require("cors");
 
 const app = express();
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contects", contactRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not Found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contack book application." });
